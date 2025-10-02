c07f89e3ecd034c0343a3083b858fffc562dd9c4
nmonjilucas2026-lgtm <nmonjilucas2026@damien.edu>
2025-10-01 23:33:24 -1000

    Create Cylinder.java

---
+++ ShapesProject/Cylinder.java
@@ -0,0 +1,98 @@
+/*
+ * This class defines the attributes and properties of a cylinder
+ */
+
+
+public class Cylinder {
+		
+	private final double pi = 3.14;
+	private double radius;
+	private double height;
+	
+	// Constructor
+	public Cylinder(double r, double h)
+	{
+		radius = r;
+		height = h;
+	}
+			
+	/*
+	 *  Returns the radius of a cylinder
+	 */
+	public double getRadius() 
+	{
+		return radius;
+	}
+	
+	/*
+	 *  Returns the height of a cylinder
+	 */
+	public double getHeight() {
+		return height;
+	}
+	
+	/*
+	 * Calculates and returns the base circumference of a cylinder
+	 */
+	public double getCircumference() 
+	{
+		return (2 * pi * radius);
+	}
+			
+	/*
+	 * Calculates and returns the base area of a cylinder
+	 */
+	public double getArea() 
+	{
+		return (pi * radius * radius);
+	}
+	
+	/*
+	 * Calculates and returns the surface area of a cylinder
+	 */
+	public double getSurfaceArea() 
+	{
+		return (2 * pi * radius * height) + (2 * pi * radius * radius);
+	}
+			
+	/*
+	 * Calculates and returns the volume of a cylinder
+	 */
+	public double getVolume() 
+	{
+		return (pi * radius * radius * height);
+	}
+		
+		
+	/*
+	 *  Returns the total degrees of a cylinder
+	 */
+	public static int getDegrees()
+	{
+		return 360;
+	}
+			
+	/*
+	 * Prints out the description of a cylinder
+	 */
+	public static void stateShape()
+	{
+		System.out.println("This class generates Cylinder shapes.");
+	}
+			
+	/*
+	 * Sets new radius
+	 */
+	public void setRadius(double newRadius) 
+	{
+		radius = newRadius;
+	}
+	
+	/*
+	 * Sets new Height
+	 */
+	public void setHeight(double newHeight) 
+	{
+		height = newHeight;
+	}	
+}
