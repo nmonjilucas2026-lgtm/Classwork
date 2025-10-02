fd0b06a147a603c4d2d85edc97636e592ac7fafc
nmonjilucas2026-lgtm <nmonjilucas2026@damien.edu>
2025-10-01 23:28:14 -1000

    Create Triangle.java

---
+++ ShapesProject/Triangle.java
@@ -0,0 +1,71 @@
+/*
+ * This class defines the attributes and properties of a triangle
+ */
+
+public class Triangle {
+
+	private double base;
+	private double height;
+
+	// Constructor
+	public Triangle(double b, double h) {
+		base = b;
+		height = h;
+	}
+
+	/*
+	 *  Returns the base of a triangle
+	 */
+	public double getBase() {
+		return base;
+	}
+
+	/*
+	 *  Returns the height of a triangle
+	 */
+	public double getHeight() {
+		return height;
+	}
+
+	/*
+	 * Calculates and returns the area of a triangle
+	 */
+	public double getArea() {
+		return (0.5 * base * height);
+	}
+
+	/*
+	 *  Calculates and returns the perimeter of a triangle
+	 */
+	public double getPerimeter() {
+		return (3 * base);
+	}
+
+	/*
+	 *  Returns the total degrees of a triangle
+	 */
+	public static int getDegrees() {
+		return 180;
+	}
+
+	/*
+	 *  Prints out the description of a triangle
+	 */
+	public static void stateShape() {
+		System.out.println("This class generates Triangular shapes.");
+	}
+	
+	/*
+	 * Sets new base
+	 */
+	public void setBase(double newBase) {
+		base = newBase;
+	}
+
+	/*
+	 *  Sets new height
+	 */
+	public void setHeight(double newHeight) {
+		height = newHeight;
+	}
+}
