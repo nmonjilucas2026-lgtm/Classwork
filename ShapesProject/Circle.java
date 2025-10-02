a240eac17f2312f5f60f179c0b1f0a80cf46d68b
nmonjilucas2026-lgtm <nmonjilucas2026@damien.edu>
2025-10-01 23:18:15 -1000

    Update Circle.java

--- ShapesProject/Circle.java
+++ ShapesProject/Circle.java
@@ -1,1 +1,66 @@
+/*
+ * This class defines the attributes and properties of a circle
+ */
+
+public class Circle {
+		
+	private final double pi = 3.14;
+	private double radius;
+		
+	
+	public Circle(double r)
+	{
+		radius = r;
+	}
+			
+	/*
+	 *  Returns the radius of a circle
+	 */
+	public double getRadius() 
+	{
+		return radius;
+	}
+			
+	/*
+	 * Calculates and returns the circumference of a circle
+	 */
+	public double getCircumference() 
+	{
+		return (2 * pi * radius);
+	}
+			
+	/*
+	 * Calculates and returns the area of a circle
+	 */
+	public double getArea() 
+	{
+		return (pi * radius * radius);
+	}
+		
+	/*
+	 *  Returns the total degrees of a circle
+	 */
+	public static int getDegrees()
+	{
+		return 360;
+	}
+			
+	/*
+	 * Prints out the description of a circle
+	 */
+	public static void stateShape()
+	{
+		System.out.println("This class generates Circular shapes.");
+	}
+			
+	/*
+	 * Sets new radius
+	 */
+	public void setRadius(double newRadius) 
+	{
+		radius = newRadius;
+	}
+}
+
+
 
