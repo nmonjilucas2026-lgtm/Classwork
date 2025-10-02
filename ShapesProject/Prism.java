bd33a8f94c4f14d1778af83ce94132ce216d7f6b
nmonjilucas2026-lgtm <nmonjilucas2026@damien.edu>
2025-10-01 23:34:33 -1000

    Create Prism.java

---
+++ ShapesProject/Prism.java
@@ -0,0 +1,86 @@
+/*
+ * This class defines the attributes and properties of a prism
+ */
+
+public class Prism {
+	
+	private double length;
+	private double width;
+	private double height;
+
+	// Constructor
+	public Prism(double l, double w, double h) {
+		length = l;
+		width = w;
+		height = h;
+	}
+
+	/*
+	 *  Returns the length of a prism
+	 */
+	public double getLength() {
+		return length;
+	}
+	
+	/*
+	 *  Returns the width of a prism
+	 */
+	public double getWidth() {
+		return width;
+	}
+
+	/*
+	 *  Returns the height of a prism
+	 */
+	public double getHeight() {
+		return height;
+	}
+
+	/*
+	 * Calculates and returns the area of a prism's base
+	 */
+	public double getArea() {
+		return (length * width);
+	}
+
+	// Calculates and returns the perimeter of a prism's base
+	public double getPerimeter() {
+		return (2 * (length + width));
+	}
+
+	// Calculates and returns the surface area of a prism
+		public double getSurfaceArea() {
+			return (2 * (length * width + length * height + width * height));
+	}
+		
+	// Calculates and returns the volume of a prism
+	public double getVolume() {
+		return (length * width * height);
+	}
+
+	// Prints out the description of a prism
+	public static void stateShape() {
+		System.out.println("This class generates Prism shapes.");
+	}
+	
+	/*
+	 * Sets new length
+	 */
+	public void setLength(double newLength) {
+		length = newLength;
+	}
+	
+	/*
+	 * Sets new width
+	 */
+	public void setWidth(double newWidth) {
+		width = newWidth;
+	}
+
+	/*
+	 *  Sets new height
+	 */
+	public void setHeight(double newHeight) {
+		height = newHeight;
+	}
+}
