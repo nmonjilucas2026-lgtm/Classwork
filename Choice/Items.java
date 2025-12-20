055ef76d634d71620409d458810dcd2dcc04ecc4
nmonjilucas2026-lgtm <nmonjilucas2026@damien.edu>
2025-12-19 19:52:38 -1000

    Update Items.java

--- Choice/Items.java
+++ Choice/Items.java
@@ -1,26 +1,32 @@
 package MakeYourChoiceGame;
 
+/*
+ * Represents an item in the game
+ */
 public class Items {
 
-    private String name;   
-    private String type;
-    private int value;   
+    private String name;  //name of the item 
+    private String type;  //type of item (heal/cure)
+    private int value;    //value of the item (healing)
     
+    //constructor to make an item
     public Items(String name, String type, int value) {
         this.name = name;
         this.type = type;
         this.value = value;
     }
  
-     
+    //returns the item name
     public String getName() { 
     	return name; 
     }
-   
+ 
+   //returns the item type
     public String getType() {
     	return type;
     }
    
+    //returns the item value
     public int getValue() { 
     	return value; 
     }
