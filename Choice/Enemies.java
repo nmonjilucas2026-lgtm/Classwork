928f4ede348e50bcd2b88bfa7a660a19003317d6
nmonjilucas2026-lgtm <nmonjilucas2026@damien.edu>
2025-12-19 19:48:45 -1000

    Update Enemies.java

--- Choice/Enemies.java
+++ Choice/Enemies.java
@@ -1,24 +1,29 @@
 package MakeYourChoiceGame;
 
+/*
+ * Represents enemies in the game
+ */
 public class Enemies {
 	
-	private int damage;	
-	private String enemyName;
 	
-		
+	private int damage;	//damage amount they can deal 
+	private String enemyName; //name of the enemy
+	
+		//constructor to create the enemy
 		public Enemies(String enemyName, int damage) {
 			this.enemyName = enemyName;
 			this.damage = damage;
 		}
 		
+		//returns the enemy name
 			public String getEnemyName() {
 			return enemyName;
 		}
-				
+			
+		//returns the enemy damage
 			public int getDamage() {
 			return damage;
 		}	
 			
 	}
-
 
