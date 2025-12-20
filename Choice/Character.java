ec0c923e00c5bbcb7d71f805f4761d1b455d3e58
nmonjilucas2026-lgtm <nmonjilucas2026@damien.edu>
2025-12-19 19:50:31 -1000

    Update Character.java

--- Choice/Character.java
+++ Choice/Character.java
@@ -1,29 +1,38 @@
 package MakeYourChoiceGame;
 
+/*
+ * Represents the user's character in game
+ */
 public class Character {
-	private int health;	
-	private String name;
 	
+	private int health;	//health value
+	private String name; //character's name
+	
+	//constructor to create a character
 	public Character(int health, String name) {
 		this.health = 100;
 		this.name = name;
 	}
 	
-	
+	//returns character name
 		public String getName() {
 		return name;
 	}
-			
+	
+	//returns character health
 		public int getHealth() {
 		return health;
 	}	
 	
+	//changes character name
 		public void setName(String newName) {	
 		name = newName;
 	}
 	
+	//changes character health
 	public void setHealth(int newHealth) {
 		health = newHealth;
 	}
 }
+
 
