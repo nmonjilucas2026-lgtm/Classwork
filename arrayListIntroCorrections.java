64230528e96531eb0b6c3ed9c49997b169842cd0
nmonjilucas2026-lgtm <nmonjilucas2026@damien.edu>
2026-05-19 14:13:10 -1000

    Update arrayListIntroCorrections.java

--- arrayListIntroCorrections.java
+++ arrayListIntroCorrections.java
@@ -1,3 +1,21 @@
+/*
+(For the following problems, you should be printing the whole ArrayList to make sure it worked)
+
+1. Instantiate an empty ArrayList that could have any type in it.
+
+2. Instantiate an empty ArrayList called intArrList that specifically stores Integers. Repeat this for Strings.
+
+3. Add 1, 2, and 3 to your intArrList one at a time so that its values are {1, 2, 3}. Then add elements to stringArrList so its values are {"Alice , "Bob" , "Charlie"}
+
+4. Add 5 to your intArrList so that its values are now {1, 2, 5, 3}. Then add "Derek" to your stringArrList so its values are {"Alice" , "Derek" , "Bob" , "Charlie"}
+
+5. Print out the second and last elements in both intArrList and stringArrList.
+
+6. Change the first element in intArrList to 0, and change the first element in stringArrList to "Zero". Print out the first element in each array before and after the change. 
+
+7. Delete the last element in both arrays. Print out what that removed element was. 
+*/
+
 import java.util.ArrayList;
 
 public class ArrayListIntro {
